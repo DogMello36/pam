6ec88bda5c99fdcc1e53a3503eb32243bd6e0f0b
DogMello36 <dogmello228@gmail.com>
2026-08-05 11:41:11 -0300

    f

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './components/Home'
 
 export default function App() {
   return (
